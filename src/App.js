e87318f7c8a4dede556056c25cd5293993274aaf
Will Linux <druids50@gmail.com>
2022-06-07 10:38:36 -0500

    basic layout wip

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="space-between">
+        <p>Memory Card Game</p>
+        <div>
+          <p>Score: </p>
+          <p>Best Score: </p>
+        </div>
+      </div>
     </div>
   );
 }
